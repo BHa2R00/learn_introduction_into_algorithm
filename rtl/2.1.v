bd4ba2fd4812c948fe2e87d4b912d88502d893eb
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2023-12-25 20:44:30 +0800

    Update 2.1.v

--- rtl/2.1.v
+++ rtl/2.1.v
@@ -9,8 +9,14 @@
  * 			i = i - 1
  * 		A[i + 1] = key
  */
+
 module insertion_sort(
-	output full, empty, idle, 
+`ifdef ASYNC
+	output locale_clock, 
+ 	output reg [3:0] nst_d, 
+`endif
+	output reg full, empty, idle, 
+ 	output reg [3:0] cst, nst, 
 	input push, pop, clear, sort, 
 	output reg [15:0] dout, 
 	input [15:0] din, 
@@ -18,82 +24,152 @@ module insertion_sort(
 	input rstn, clk 
 );
 
-`ifndef gray
-`define gray(x) (x^(x>>1))
+reg push_d, pop_d, clear_d, sort_d;
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) begin
+		push_d <= 1'b0;
+		pop_d <= 1'b0;
+		clear_d <= 1'b0;
+		sort_d <= 1'b0;
+	end
+	else if(enable) begin
+		push_d <= push;
+		pop_d <= pop;
+		clear_d <= clear;
+		sort_d <= sort;
+	end
+end
+wire push_x = push ^ push_d;
+wire pop_x = pop ^ pop_d;
+wire clear_x = clear ^ clear_d;
+wire sort_x = sort ^ sort_d;
+
+`ifndef GRAY
+	`define GRAY(X) (X^(X>>1))
 `endif
- reg [3:0] cst;
 localparam [3:0] 
-	st_do_i_end	= `gray(11), st_do_i	= `gray(10), st_do_i_jmp	= `gray(9), st_do_i_init	= `gray(8), 
-	st_do_j_end	= `gray(7),  st_do_j	= `gray(6),  st_do_j_jmp	= `gray(5), st_do_j_init	= `gray(4),
-	st_pop	 	= `gray(3),  st_push 	= `gray(2),  st_clear 		= `gray(1),
-	st_idle 	= 0;
-assign idle = cst == st_idle;
+	st_do_i_end	= `GRAY(11), st_do_i	= `GRAY(10), st_do_i_jmp	= `GRAY(9), st_do_i_init	= `GRAY(8), 
+	st_do_j_end	= `GRAY(7),  st_do_j	= `GRAY(6),  st_do_j_jmp	= `GRAY(5), st_do_j_init	= `GRAY(4),
+	st_pop	 	= `GRAY(3),  st_push 	= `GRAY(2),  st_clear 		= `GRAY(1),
+	st_idle 	= `GRAY(0);
 
 reg [15:0] A[0:255];
 reg [7:0] p, j, i;
-assign full = p == 8'd255;
-assign empty = p == 8'd0;
 reg [15:0] key;
 wire [7:0] pop_p = p - 8'd1;
 
-reg [1:0] push_d, pop_d, clear_d, sort_d;
-always@(negedge rstn or posedge clk) begin
-	if(!rstn) begin
-		push_d <= 2'b00;
-		pop_d <= 2'b00;
-		clear_d <= 2'b00;
-		sort_d <= 2'b00;
-	end
-	else if(enable) begin
-		push_d[1] <= push_d[0]; push_d[0] <= push;
-		pop_d[1] <= pop_d[0]; pop_d[0] <= pop;
-		clear_d[1] <= clear_d[0]; clear_d[0] <= clear;
-		sort_d[1] <= sort_d[0]; sort_d[0] <= sort;
+`ifdef ASYNC
+assign locale_clock = |(nst_d ^ cst);
+always@(*) begin
+	if(!rstn) nst_d = st_idle;
+	else if(!clk) begin
+		if(enable) nst_d = nst;
 	end
 end
-wire push_x = ^push_d;
-wire pop_x = ^pop_d;
-wire clear_x = ^clear_d;
-wire sort_x = ^sort_d;
+always@(*) begin
+	if(!rstn) cst = st_idle;
+	else if(clk) begin
+		if(enable) cst = nst_d;
+	end
+end
+`else
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) cst <= st_idle;
+	else if(enable) cst <= nst;
+end
+`endif
+
+always@(*) begin
+	case(cst)
+		st_idle: nst = clear_x ? st_clear : push_x ? st_push : pop_x ? st_pop : sort_x ? st_do_j_init : cst;
+		st_clear: nst = st_idle;
+		st_push: nst = st_idle;
+		st_pop: nst = st_idle;
+		st_do_j_init: nst = st_do_j_jmp;
+		st_do_j_jmp: nst = (j == p) ? st_do_j_end : st_do_i_init;
+		st_do_i_init: nst = st_do_i_jmp;
+		st_do_i_jmp: nst = ((i == -8'd1) || (A[i] < key)) ? st_do_i_end : st_do_i;
+		st_do_i: nst = st_do_i_jmp;
+		st_do_i_end: nst = st_do_j;
+		st_do_j: nst = st_do_j_jmp;
+		st_do_j_end: nst = st_idle;
+		default: nst = st_idle;
+	endcase
+end
+
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) p <= 8'd0;
+	else if(enable) begin
+		case(nst)
+			st_clear: p <= 8'd0;
+			st_push: p <= p + 8'd1;
+			st_pop: p <= p - 8'd1;
+			st_do_j_init: p <= p - 1'd1;
+			st_do_j_end: p <= p - 1'd1;
+			default: p <= p;
+		endcase
+	end
+end
+
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) j <= 8'd0;
+	else if(enable) begin
+		case(nst)
+			st_do_j_init: j <= 8'd1;
+			st_do_j: j <= j + 8'd1;
+			default: j <= j;
+		endcase
+	end
+end
+
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) i <= 8'd0;
+	else if(enable) begin
+		case(nst)
+			st_do_i_init: i <= j - 8'd1;
+			st_do_i: i <= i - 8'd1;
+			default: i <= i;
+		endcase
+	end
+end
 
 always@(negedge rstn or posedge clk) begin
 	if(!rstn) begin
 		dout <= 16'd0;
-		cst <= st_idle;
-		p <= 8'd0; j <= 8'd0; i <= 8'd0;
 		key <= 16'd0;
 	end
 	else if(enable) begin
-		case(cst)
-			st_idle: begin
-				if(clear_x) cst <= st_clear;
-				else if(push_x) cst <= st_push;
-				else if(pop_x) cst <= st_pop;
-				else if(sort_x) cst <= st_do_j_init;
-			end
-			st_clear: begin cst <= st_idle; p <= 8'd0; end
-			st_push: begin cst <= st_idle; p <= p + 8'd1; A[p] <= din; end
-			st_pop: begin cst <= st_idle; p <= p - 8'd1; dout <= A[pop_p]; end
-			st_do_j_init: begin cst <= st_do_j_jmp; j <= 8'd1; p <= p - 1'd1; end
-			st_do_j_jmp: begin
-				 key <= A[j];
-				if(j == p) cst <= st_do_j_end;
-				else cst <= st_do_i_init;
-			end
-			st_do_i_init: begin cst <= st_do_i_jmp; i <= j - 8'd1; end
-			st_do_i_jmp: begin
-				if((i == -8'd1) || (A[i] < key)) cst <= st_do_i_end;
-				else cst <= st_do_i;
-			end
-			st_do_i: begin cst <= st_do_i_jmp; i <= i - 8'd1; A[i + 8'd1] <= A[i]; end
-			st_do_i_end: begin cst <= st_do_j; A[i + 8'd1] <= key; end
-			st_do_j: begin cst <= st_do_j_jmp; j <= j + 8'd1; end
-			st_do_j_end: begin cst <= st_idle; p <= p - 1'd1; end
+		case(nst)
+			st_push: A[p] <= din;
+			st_pop: dout <= A[pop_p];
+			st_do_j_jmp: key <= A[j];
+			st_do_i: A[i + 8'd1] <= A[i];
+			st_do_i_end: A[i + 8'd1] <= key;
 			default: begin
-				cst <= st_idle;
 				dout <= dout;
-				p <= p; j <= j; i <= i;
 				key <= key;
+			end
+		endcase
+	end
+end
+
+always@(negedge rstn or posedge clk) begin
+	if(!rstn) begin
+		idle <= 1'b1;
+		full <= 1'b0;
+		empty <= 1'b1;
+	end
+	else if(enable) begin
+		case(nst)
+			st_idle: begin
+				idle <= 1'b1;
+				full <= p == 8'd255;
+				empty <= p == 8'd0;
+			end
+			default: begin
+				idle <= 1'b0;
+				full <= full;
+				empty <= empty;
 			end
 		endcase
 	end
